52ce561ff0f6bc65c412f02d449f1df57725ef6d
Alex Muharski <aom9@mail.ru>
2017-11-02 20:19:21 +0300

    Completed task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -19,11 +19,44 @@ class TicTacToe {
     }
 
     isFinished() {
-
+        if(this.getWinner() || this.isDraw()){
+            return true;
+        }
+        return false;
     }
 
     getWinner() {
-
+        let counters = [0,0,0,0];
+        for(let i = 0; i < 3; i++){
+            counters.fill(0);
+            for(let j = 0; j < 2; j++ ){
+                if(this.field[i][j] === this.field[i][j + 1]){
+                    counters[0]++;
+                    if(counters[0] === 2){
+                        return this.field[i][j];
+                    }
+                }
+                if(this.field[j][i] === this.field[j + 1][i]){
+                    counters[1]++;
+                    if(counters[1] === 2){
+                        return this.field[j][i];
+                    }
+                }
+                if(this.field[j][j] === this.field[j + 1][j + 1]){
+                    counters[2]++;
+                    if(counters[2] === 2){
+                        return this.field[j][j];
+                    }
+                }
+                if(this.field[j][2 - j] === this.field[j + 1][1 - j]){
+                    counters[3]++;
+                    if(counters[3] === 2){
+                        return this.field[j][2-j];
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     noMoreTurns() {
@@ -36,7 +69,10 @@ class TicTacToe {
     }
 
     isDraw() {
-
+        if(this.getWinner() === null && this.noMoreTurns() ){
+            return true;
+        }
+            return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
